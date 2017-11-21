c8d3d2937ae517a5f57ef574ab493937b9640bde
Mikhail Kalinouski <1135323@gmail.com>
2017-11-21 23:39:25 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,24 @@
 package ru.job4j;
 
+
+/**
+* Class Класс для печати "Hello world"
+* @author kalinouskij
+* @since 21.11.2017
+* @version 1
+*/
+
+
 public class Calculate {
 	
+	/**
+	* вывод в консоль "Hello world"
+	* @param arg - arg.
+	*/
+	
 	public static void main (String[] args){
-		System.out.println ("Hello warld.");
+		System.out.println ("Hello world.");
 	}
+	
+	
 }
