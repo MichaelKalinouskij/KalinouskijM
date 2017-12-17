f6fec603633a7cf817882e016838ffbbfbb93478
Mikhail Kalinouski <1135323@gmail.com>
2017-12-17 16:35:47 +0300

    3.2. Максимум из двух чисел [#189][#32835](исправление)

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,4 +1,28 @@
 package ru.job4j.loop;
 
+
+
 public class Counter {
-}
+    int start;
+    int finish;
+
+
+
+
+
+        public int add (int start, int finish) {
+            this.start = start;
+            this.finish = finish;
+            int result = 0;
+
+
+            for (start = 2; start <= finish; start++) {
+                if (start % 2 == 0) {
+                    result = result + start;
+                }
+            }
+            return result;
+        }
+    }
+
+
--- chapter_001/src/main/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/main/java/ru/job4j/loop/CounterTest.java
@@ -1,4 +1,19 @@
 package ru.job4j.loop;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+
 public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThirty(){
+        Counter sum = new Counter();
+        sum.add(1,10);
+        assertThat(sum, is(2+4+6+8+10));
+
+    }
 }
